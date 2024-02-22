72585c12aa0a5c17b362a73ac2f0d65b5bdac838
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2024-02-22 21:28:27 +0000

    Moved plugins to it's own directory, set tabs and size.

---
+++ neovim/.config/nvim/plugins.lua
@@ -0,0 +1,28 @@
+-- Get and Update lazy.nvim.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+
+	-- HARPOOOOOOOOOON 2.0!
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" }
+	}
+	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
+	"rebelot/kanagawa.nvim",
+	"bluz71/vim-nightfly-colors",
+})
