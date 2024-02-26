7c31d44d073d7ea48fc06583fcdf88a2567135fe
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2024-02-26 16:47:13 +0000

    New mapping: Middle click pastes in cursor pos, not mouse pos.

--- neovim/.config/nvim/lua/nvimconfig/mappings.lua
+++ neovim/.config/nvim/lua/nvimconfig/mappings.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Pasting with middle mouse pastes in cursor position, not mouse position.
+vim.keymap.set("n", "<MiddleMouse>", "i<MiddleMouse><Esc>")
