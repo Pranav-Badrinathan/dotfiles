8e98cea9778aa58805baaaca7dcadd2dcd2638db
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2024-02-28 19:31:37 +0000

    Some more nvim settings and keymappings.

--- neovim/.config/nvim/lua/nvimconfig/mappings.lua
+++ neovim/.config/nvim/lua/nvimconfig/mappings.lua
@@ -3,3 +3,23 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 -- Pasting with middle mouse pastes in cursor position, not mouse position.
 vim.keymap.set("n", "<MiddleMouse>", "i<MiddleMouse><Esc>")
+
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-Up>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-Down>", ":m '<-2<CR>gv=gv")
+
+-- Same as 'J', but cursor stays in place. See ":help J".
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("v", "J", "mzJ`z")
+
+-- Paste without taking the thing pasted over into the paste buffer.
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Copy into OS clipboard/register
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+-- Delete into void register.
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
--- neovim/.config/nvim/lua/nvimconfig/settings.lua
+++ neovim/.config/nvim/lua/nvimconfig/settings.lua
@@ -1,5 +1,7 @@
 local opt = vim.opt
 
+opt.termguicolors = true
+
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = false -- This is default, but still set explicitly
@@ -9,5 +11,11 @@ opt.undodir = vim.fn.stdpath("config") .. "/.undo"
 opt.undofile = true
 
 opt.relativenumber = true
-
+opt.scrolloff = 6
 opt.signcolumn = "yes:1"
+opt.colorcolumn = {81, 121}
+
+opt.wrap = false
+opt.incsearch = true
+opt.updatetime = 100
+opt.splitbelow = true
