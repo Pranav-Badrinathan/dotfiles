a7bd5b4e38e86fbb881f3a12b444c1ed6e2c054d
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2024-02-28 22:06:19 -0500

    Do not need to provide an explicit undo dir.
    Just setting undofile to true will cause nvim to use the default undo
    dir, in "~/.local/share/nvim/undo" by default on Linux, and
    "~/AppData/Local/nvim-data/undo" on Windows. Just use this!

--- neovim/.config/nvim/lua/nvimconfig/settings.lua
+++ neovim/.config/nvim/lua/nvimconfig/settings.lua
@@ -7,7 +7,7 @@ opt.shiftwidth = 4
 opt.expandtab = false -- This is default, but still set explicitly
 
 -- Set vim persistent undo.
-opt.undodir = vim.fn.stdpath("config") .. "/.undo"
+-- opt.undodir = vim.fn.stdpath("config") .. "/.undo"
 opt.undofile = true
 
 opt.relativenumber = true
